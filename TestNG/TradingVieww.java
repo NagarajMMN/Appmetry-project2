cdabbe5db4e28034a58a0ebb6e568e5429d7eaff
NagarajMMN <105267768+NagarajMMN@users.noreply.github.com>
2022-09-22 16:48:25 +0530

    Add files via upload

---
+++ TestNG/TradingVieww.java
@@ -0,0 +1,51 @@
+package TestNG;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TradingVieww {
+	ChromeDriver driver;
+	@Test 
+	 public void Trade() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		 ChromeDriver driver=new ChromeDriver();
+		 Trading tradee=new Trading(driver);
+		 driver.get("https://in.tradingview.com/");
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		tradee.ClickMarket();
+		tradee.ClickStock();
+		tradee.ClickQuotes();
+		Thread.sleep(10000);
+		tradee.ClickToploser();
+		System.out.println();
+		System.out.println("Strong Buy==================");
+		List<WebElement> lbuy = driver.findElements(By.xpath("//div[text()='Strong Buy']//preceding::sup[1]"));
+			 for (WebElement e : lbuy) {
+		 System.out.println(e.getText());
+	}
+	 System.out.println();
+	 System.out.println("Strong BUY==================");
+	 tradee.ClickMostactive();
+	 	List<WebElement> abuy = driver.findElements(By.xpath("//div[text()='Strong Buy']//preceding::sup[1]"));
+
+	   for(WebElement sell1 : abuy) {
+	  		 System.out.println(sell1.getText());			
+		 			    
+	}
+	System.out.println();
+	System.out.println("matching=======");
+	System.out.println(lbuy!=abuy);
+	
+		 
+//	Thread.sleep(10000);
+//	driver.quit();
+	}
+}
